cb86e3fd1a00a6ecdab3b7dfdefb537102fd554c
Deivax <>
2022-04-22 17:17:26 +0200

    dummy openconversation

---
+++ src/views/console/OpenConversationView.ts
@@ -0,0 +1,7 @@
+import { WithConsoleView } from "./WithConsoleView";
+
+export class OpenConversationView extends WithConsoleView {
+  public render(): void {
+    this.console.print("openning conversation... work in progress! bye");
+  }
+}
