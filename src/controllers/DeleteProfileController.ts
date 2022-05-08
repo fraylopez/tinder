c9ac99d07927b8a1a0e94f99d963515f071765c4
Josue Alcantara <josue.almo@gmail.com>
2022-05-08 10:49:15 +0200

    chore: all controllers implements controller view

--- src/controllers/DeleteProfileController.ts
+++ src/controllers/DeleteProfileController.ts
@@ -1,9 +1,8 @@
 import { FileSystemProfilePersistenceService } from "../infrastructure/file-system/FileSystemProfilePersistenceService";
+import { Controller } from "./Controller";
 
-export class DeleteProfileController {
-  constructor(
-    private persistenceService: FileSystemProfilePersistenceService,
-  ) {}
+export class DeleteProfileController implements Controller<[name: string], void> {
+  constructor(private persistenceService: FileSystemProfilePersistenceService) {}
 
   public control(name: string): void {
     const profile = this.persistenceService.find(name);
