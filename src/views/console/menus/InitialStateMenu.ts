bad839c742586bc3b970fb3d2fe48f7c37133683
Josue Alcantara <josue.almo@gmail.com>
2022-05-23 23:08:40 +0200

    chore: moving file

--- src/views/console/menus/InitialStateMenu.ts
+++ src/views/console/menus/InitialStateMenu.ts
@@ -1,7 +1,7 @@
 import { InitialStateController } from "../../../controllers/state/InitialStateController";
 import { Session } from "../../../models/Session";
 import { Menu } from "./Menu";
-import { LoginMenuItem } from "./LoginMenuItem";
+import { LoginMenuItem } from "./items/LoginMenuItem";
 import { CreateProfileMenuItem } from "./items/CreateProfileMenuItem";
 
 export class InitialStateMenu extends Menu {
